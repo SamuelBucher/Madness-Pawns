20a6f529e0a4060d96380f7c6c4d8e6fe5141512
Maxim Khannanov <maksimka.khannanov@mail.ru>
2022-10-22 01:19:15 +0300

    fixed head offset for adult pawns

--- Madness Pawns/Source/Madness Pawns/Class1.cs
+++ Madness Pawns/Source/Madness Pawns/Class1.cs
@@ -35,16 +35,6 @@ namespace Madness_Pawns
             }
         }
 
-        /*[HarmonyPatch(typeof(PawnSkinColors), "GetSkinColor")]
-        class GetSkinColorPatch
-        {
-            [HarmonyPostfix]
-            public static Color GetSkinColorPostfix(Color result)
-            {
-                return new Color(192f / 255f, 190f / 255f, 188f / 255f);
-            }
-        }*/
-
         [HarmonyPatch(typeof(Pawn_StoryTracker))]
         [HarmonyPatch("SkinColor", MethodType.Getter)]
         public static class SkinColorPatch
@@ -108,43 +98,47 @@ namespace Madness_Pawns
             }
         }
 
-        [HarmonyPatch(typeof(PawnGraphicSet), "ResolveAllGraphics")]
-        class ResolveAllGraphicsPatch
+        [HarmonyPatch(typeof(PawnRenderer), "BaseHeadOffsetAt")]
+        class BaseHeadOffsetAtPatch
         {
-            [HarmonyPostfix]
-            public static void ResolveAllGraphicsPostfix(ref PawnGraphicSet __instance)
+            [HarmonyPrefix]
+            public static bool BaseHeadOffsetAtPrefix(ref PawnRenderer __instance, ref Vector3 __result, Rot4 rotation)
             {
+                FieldInfo fld = typeof(PawnRenderer).GetField("pawn");
+                Pawn instancePawn = (Pawn)fld.GetValue(__instance);
 
-                if (__instance.pawn.RaceProps.Humanlike)
+                BodyTypeDef bodyType;
+
+                if (instancePawn.gender == Gender.Female && LoadedModManager.GetMod<MadnessPawns>().GetSettings<Settings>().enableFemaleBodyType)
+                    bodyType = BodyTypeDefOf.Female;
+                else
+                    bodyType = BodyTypeDefOf.Male;
+
+                Vector2 vector = bodyType.headOffset * Mathf.Sqrt(instancePawn.ageTracker.CurLifeStage.bodySizeFactor);
+                switch (rotation.AsInt)
                 {
-                    string bodyTypePath;
-
-                    if (__instance.pawn.gender == Gender.Female && LoadedModManager.GetMod<MadnessPawns>().GetSettings<Settings>().enableFemaleBodyType)
-                        bodyTypePath = BodyTypeDefOf.Female.bodyNakedGraphicPath;
-                    else
-                        bodyTypePath = BodyTypeDefOf.Male.bodyNakedGraphicPath;
-
-                    Color color = __instance.pawn.story.SkinColorOverriden ? (PawnGraphicSet.RottingColorDefault * __instance.pawn.story.SkinColor) : PawnGraphicSet.RottingColorDefault;
-
-                    __instance.nakedGraphic = GraphicDatabase.Get<Graphic_Multi>(bodyTypePath, ShaderUtility.GetSkinShader(__instance.pawn.story.SkinColorOverriden), Vector2.one, __instance.pawn.story.SkinColor);
-                    __instance.rottingGraphic = GraphicDatabase.Get<Graphic_Multi>(bodyTypePath, ShaderUtility.GetSkinShader(__instance.pawn.story.SkinColorOverriden), Vector2.one, color);
-                    __instance.dessicatedGraphic = GraphicDatabase.Get<Graphic_Multi>(bodyTypePath, ShaderDatabase.Cutout);
-                    __instance.headStumpGraphic = null;
-
-                    if ((__instance.pawn.style != null && ModsConfig.IdeologyActive) && (__instance.pawn.style.BodyTattoo != null && __instance.pawn.style.BodyTattoo != TattooDefOf.NoTattoo_Body))
-                    {
-                        Color skinColor = __instance.pawn.story.SkinColor;
-                        skinColor.a *= 0.8f;
-                        __instance.bodyTattooGraphic = GraphicDatabase.Get<Graphic_Multi>(__instance.pawn.style.BodyTattoo.texPath, ShaderDatabase.CutoutSkinOverlay, Vector2.one, skinColor, Color.white, null, bodyTypePath);
-                    }
-                    else
-                        __instance.bodyTattooGraphic = null;
+                    case 0:
+                        __result =  new Vector3(0f, 0f, vector.y);
+                        return false;
+                    case 1:
+                        __result = new Vector3(vector.x, 0f, vector.y);
+                        return false;
+                    case 2:
+                        __result = new Vector3(0f, 0f, vector.y);
+                        return false;
+                    case 3:
+                        __result = new Vector3(-vector.x, 0f, vector.y);
+                        return false;
+                    default:
+                        Log.Error("BaseHeadOffsetAt error in " + instancePawn);
+                        __result = Vector3.zero;
+                        return false;
                 }
             }
         }
 
         [HarmonyPatch(typeof(PawnGraphicSet), "ResolveApparelGraphics")]
-        class ResolveApparelGraphics
+        class ResolveApparelGraphicsPatch
         {
             [HarmonyPrefix]
             public static bool ResolveApparelGraphicsPrefix(ref PawnGraphicSet __instance)
@@ -152,19 +146,19 @@ namespace Madness_Pawns
                 __instance.ClearCache();
                 __instance.apparelGraphics.Clear();
 
-                BodyTypeDef bodyTypePath;
+                BodyTypeDef bodyType;
 
                 if (__instance.pawn.gender == Gender.Female && LoadedModManager.GetMod<MadnessPawns>().GetSettings<Settings>().enableFemaleBodyType)
-                    bodyTypePath = BodyTypeDefOf.Female;
+                    bodyType = BodyTypeDefOf.Female;
                 else
-                    bodyTypePath = BodyTypeDefOf.Male;
+                    bodyType = BodyTypeDefOf.Male;
 
                 using (List<Apparel>.Enumerator enumerator = __instance.pawn.apparel.WornApparel.GetEnumerator())
                 {
                     while (enumerator.MoveNext())
                     {
                         ApparelGraphicRecord item;
-                        if (ApparelGraphicRecordGetter.TryGetGraphicApparel(enumerator.Current, bodyTypePath, out item))
+                        if (ApparelGraphicRecordGetter.TryGetGraphicApparel(enumerator.Current, bodyType, out item))
                         {
                             __instance.apparelGraphics.Add(item);
                         }
@@ -175,6 +169,53 @@ namespace Madness_Pawns
             }
         }
     }
+
+    [HarmonyPatch(typeof(PawnGraphicSet), "ResolveAllGraphics")]
+        class ResolveAllGraphicsPatch
+        {
+            [HarmonyPostfix]
+            public static void ResolveAllGraphicsPostfix(ref PawnGraphicSet __instance)
+            {
+                if (__instance.pawn.RaceProps.Humanlike)
+                {
+                    //string bodyDessicatedGraphicPath;
+
+                    /*if (__instance.pawn.gender == Gender.Female && LoadedModManager.GetMod<MadnessPawns>().GetSettings<Settings>().enableFemaleBodyType)
+                    {
+                        bodyNakedGraphicPath = "Things/Pawn/Humanlike/Bodies/Naked_Female";
+                        bodyDessicatedGraphicPath = "Things/Pawn/Humanlike/Bodies/Dessicated/Dessicated_Female";
+                    }
+                    else
+                    {
+                        bodyNakedGraphicPath = "Things/Pawn/Humanlike/Bodies/Naked_Male";
+                        bodyDessicatedGraphicPath = "Things/Pawn/Humanlike/Bodies/Dessicated/Dessicated_Male";
+                    }*/
+
+                    BodyTypeDef bodyType;
+
+                    if (__instance.pawn.gender == Gender.Female && LoadedModManager.GetMod<MadnessPawns>().GetSettings<Settings>().enableFemaleBodyType)
+                        bodyType = BodyTypeDefOf.Female;
+                    else
+                        bodyType = BodyTypeDefOf.Male;
+
+                    Color color = __instance.pawn.story.SkinColorOverriden ? (PawnGraphicSet.RottingColorDefault * __instance.pawn.story.SkinColor) : PawnGraphicSet.RottingColorDefault;
+
+                    __instance.nakedGraphic = GraphicDatabase.Get<Graphic_Multi>(bodyType.bodyNakedGraphicPath, ShaderUtility.GetSkinShader(__instance.pawn.story.SkinColorOverriden), Vector2.one, __instance.pawn.story.SkinColor);
+                    __instance.rottingGraphic = GraphicDatabase.Get<Graphic_Multi>(bodyType.bodyNakedGraphicPath, ShaderUtility.GetSkinShader(__instance.pawn.story.SkinColorOverriden), Vector2.one, color);
+                    __instance.dessicatedGraphic = GraphicDatabase.Get<Graphic_Multi>(bodyType.bodyDessicatedGraphicPath, ShaderDatabase.Cutout);
+                    __instance.headStumpGraphic = null;
+
+                    if ((__instance.pawn.style != null && ModsConfig.IdeologyActive) && (__instance.pawn.style.BodyTattoo != null && __instance.pawn.style.BodyTattoo != TattooDefOf.NoTattoo_Body))
+                    {
+                        Color skinColor = __instance.pawn.story.SkinColor;
+                        skinColor.a *= 0.8f;
+                        __instance.bodyTattooGraphic = GraphicDatabase.Get<Graphic_Multi>(__instance.pawn.style.BodyTattoo.texPath, ShaderDatabase.CutoutSkinOverlay, Vector2.one, skinColor, Color.white, null, bodyType.bodyNakedGraphicPath);
+                    }
+                    else
+                        __instance.bodyTattooGraphic = null;
+                }
+            }
+        }
 
     public class Settings : ModSettings
     {
