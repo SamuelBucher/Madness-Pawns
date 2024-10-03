b7fa69ec323a41e0ab2e29142c53935e434d941d
Samuel Bucher <maxwellgrey853@gmail.com>
2024-10-03 10:21:18 +0300

    Fixed error spam and pawns not being drawn when Biotech and/or Anomly DLC are missing/disabled

--- Source/Madness Pawns 1.5/MP_Cache.cs
+++ Source/Madness Pawns 1.5/MP_Cache.cs
@@ -1,84 +1,100 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using Verse;
 
 namespace Madness_Pawns
 {
+    [StaticConstructorOnStartup]
     public static class MP_Cache
     {
-        public static Dictionary<HeadTypeDef,HeadTypeDef> HeadTypeCacheMale = new Dictionary<HeadTypeDef, HeadTypeDef>()
-        {
-            //Vanilla
-            { MP_HeadTypeDefOf.Male_AverageNormal, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Male_AveragePointy, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Male_AverageWide, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Male_NarrowNormal, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Male_NarrowPointy, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Male_NarrowWide, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Female_AverageNormal, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Female_AveragePointy, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Female_AverageWide, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Female_NarrowNormal, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Female_NarrowPointy, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Female_NarrowWide, MP_HeadTypeDefOf.Grunt_Male },
-            //Biotech
-            { MP_HeadTypeDefOf.Gaunt, MP_HeadTypeDefOf.Grunt_Male_Gaunt },
-            { MP_HeadTypeDefOf.Male_HeavyJawNormal, MP_HeadTypeDefOf.Grunt_Male_Heavy },
-            { MP_HeadTypeDefOf.Female_HeavyJawNormal, MP_HeadTypeDefOf.Grunt_Male_Heavy },
-            { MP_HeadTypeDefOf.Furskin_Average1, MP_HeadTypeDefOf.Grunt_Male_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Average2, MP_HeadTypeDefOf.Grunt_Male_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Average3, MP_HeadTypeDefOf.Grunt_Male_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Narrow1, MP_HeadTypeDefOf.Grunt_Male_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Narrow2, MP_HeadTypeDefOf.Grunt_Male_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Narrow3, MP_HeadTypeDefOf.Grunt_Male_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Gaunt, MP_HeadTypeDefOf.Grunt_Male_Furskin_Gaunt },
-            { MP_HeadTypeDefOf.Furskin_Heavy1, MP_HeadTypeDefOf.Grunt_Male_Furskin_Heavy },
-            { MP_HeadTypeDefOf.Furskin_Heavy2, MP_HeadTypeDefOf.Grunt_Male_Furskin_Heavy },
-            { MP_HeadTypeDefOf.Furskin_Heavy3, MP_HeadTypeDefOf.Grunt_Male_Furskin_Heavy },
-            //Anomaly
-            { MP_HeadTypeDefOf.Ghoul_Normal, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Ghoul_Heavy, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Ghoul_Narrow, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Ghoul_Wide, MP_HeadTypeDefOf.Grunt_Male_Heavy },
-            { MP_HeadTypeDefOf.CultEscapee, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.TimelessOne, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.DarkScholar_Female, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.DarkScholar_Male, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Leathery_Female, MP_HeadTypeDefOf.Grunt_Male },
-            { MP_HeadTypeDefOf.Leathery_Male, MP_HeadTypeDefOf.Grunt_Male }
-        };
+        public static Dictionary<HeadTypeDef, HeadTypeDef> HeadTypeCacheMale = new Dictionary<HeadTypeDef, HeadTypeDef>()
+        { };
 
-        public static Dictionary<HeadTypeDef,HeadTypeDef> HeadTypeCacheFemale = new Dictionary<HeadTypeDef, HeadTypeDef>()
+        public static Dictionary<HeadTypeDef, HeadTypeDef> HeadTypeCacheFemale = new Dictionary<HeadTypeDef, HeadTypeDef>()
+        { };
+
+        static MP_Cache()
         {
-            //Vanilla
-            { MP_HeadTypeDefOf.Female_AverageNormal, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Female_AveragePointy, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Female_AverageWide, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Female_NarrowNormal, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Female_NarrowPointy, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Female_NarrowWide, MP_HeadTypeDefOf.Grunt_Female },
-            //Biotech
-            { MP_HeadTypeDefOf.Gaunt, MP_HeadTypeDefOf.Grunt_Female_Gaunt },
-            { MP_HeadTypeDefOf.Female_HeavyJawNormal, MP_HeadTypeDefOf.Grunt_Female_Heavy },
-            { MP_HeadTypeDefOf.Furskin_Average1, MP_HeadTypeDefOf.Grunt_Female_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Average2, MP_HeadTypeDefOf.Grunt_Female_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Average3, MP_HeadTypeDefOf.Grunt_Female_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Narrow1, MP_HeadTypeDefOf.Grunt_Female_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Narrow2, MP_HeadTypeDefOf.Grunt_Female_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Narrow3, MP_HeadTypeDefOf.Grunt_Female_Furskin },
-            { MP_HeadTypeDefOf.Furskin_Gaunt, MP_HeadTypeDefOf.Grunt_Female_Furskin_Gaunt },
-            { MP_HeadTypeDefOf.Furskin_Heavy1, MP_HeadTypeDefOf.Grunt_Female_Furskin_Heavy },
-            { MP_HeadTypeDefOf.Furskin_Heavy2, MP_HeadTypeDefOf.Grunt_Female_Furskin_Heavy },
-            { MP_HeadTypeDefOf.Furskin_Heavy3, MP_HeadTypeDefOf.Grunt_Female_Furskin_Heavy },
-            //Anomaly
-            { MP_HeadTypeDefOf.Ghoul_Normal, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Ghoul_Heavy, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Ghoul_Narrow, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Ghoul_Wide, MP_HeadTypeDefOf.Grunt_Female_Heavy },
-            { MP_HeadTypeDefOf.CultEscapee, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.TimelessOne, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.DarkScholar_Female, MP_HeadTypeDefOf.Grunt_Female },
-            { MP_HeadTypeDefOf.Leathery_Female, MP_HeadTypeDefOf.Grunt_Female }
-        };
+            HeadTypeCacheMale = new Dictionary<HeadTypeDef, HeadTypeDef>()
+            {
+                { MP_HeadTypeDefOf.Male_AverageNormal, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Male_AveragePointy, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Male_AverageWide, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Male_NarrowNormal, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Male_NarrowPointy, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Male_NarrowWide, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Female_AverageNormal, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Female_AveragePointy, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Female_AverageWide, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Female_NarrowNormal, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Female_NarrowPointy, MP_HeadTypeDefOf.Grunt_Male },
+                { MP_HeadTypeDefOf.Female_NarrowWide, MP_HeadTypeDefOf.Grunt_Male }
+            };
+
+            HeadTypeCacheFemale = new Dictionary<HeadTypeDef, HeadTypeDef>()
+            {
+                { MP_HeadTypeDefOf.Female_AverageNormal, MP_HeadTypeDefOf.Grunt_Female },
+                { MP_HeadTypeDefOf.Female_AveragePointy, MP_HeadTypeDefOf.Grunt_Female },
+                { MP_HeadTypeDefOf.Female_AverageWide, MP_HeadTypeDefOf.Grunt_Female },
+                { MP_HeadTypeDefOf.Female_NarrowNormal, MP_HeadTypeDefOf.Grunt_Female },
+                { MP_HeadTypeDefOf.Female_NarrowPointy, MP_HeadTypeDefOf.Grunt_Female },
+                { MP_HeadTypeDefOf.Female_NarrowWide, MP_HeadTypeDefOf.Grunt_Female }
+            };
+
+            if (ModsConfig.BiotechActive)
+            {
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Gaunt, MP_HeadTypeDefOf.Grunt_Male_Gaunt);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Male_HeavyJawNormal, MP_HeadTypeDefOf.Grunt_Male_Heavy);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Female_HeavyJawNormal, MP_HeadTypeDefOf.Grunt_Male_Heavy);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Average1, MP_HeadTypeDefOf.Grunt_Male_Furskin);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Average2, MP_HeadTypeDefOf.Grunt_Male_Furskin);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Average3, MP_HeadTypeDefOf.Grunt_Male_Furskin);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Narrow1, MP_HeadTypeDefOf.Grunt_Male_Furskin);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Narrow2, MP_HeadTypeDefOf.Grunt_Male_Furskin);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Narrow3, MP_HeadTypeDefOf.Grunt_Male_Furskin);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Gaunt, MP_HeadTypeDefOf.Grunt_Male_Furskin_Gaunt);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Heavy1, MP_HeadTypeDefOf.Grunt_Male_Furskin_Heavy);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Heavy2, MP_HeadTypeDefOf.Grunt_Male_Furskin_Heavy);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Furskin_Heavy3, MP_HeadTypeDefOf.Grunt_Male_Furskin_Heavy);
+
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Gaunt, MP_HeadTypeDefOf.Grunt_Female_Gaunt);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Female_HeavyJawNormal, MP_HeadTypeDefOf.Grunt_Female_Heavy);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Average1, MP_HeadTypeDefOf.Grunt_Female_Furskin);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Average2, MP_HeadTypeDefOf.Grunt_Female_Furskin);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Average3, MP_HeadTypeDefOf.Grunt_Female_Furskin);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Narrow1, MP_HeadTypeDefOf.Grunt_Female_Furskin);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Narrow2, MP_HeadTypeDefOf.Grunt_Female_Furskin);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Narrow3, MP_HeadTypeDefOf.Grunt_Female_Furskin);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Gaunt, MP_HeadTypeDefOf.Grunt_Female_Furskin_Gaunt);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Heavy1, MP_HeadTypeDefOf.Grunt_Female_Furskin_Heavy);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Heavy2, MP_HeadTypeDefOf.Grunt_Female_Furskin_Heavy);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Furskin_Heavy3, MP_HeadTypeDefOf.Grunt_Female_Furskin_Heavy);
+            }
+
+            if (ModsConfig.AnomalyActive)
+            {
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Ghoul_Normal, MP_HeadTypeDefOf.Grunt_Male);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Ghoul_Heavy, MP_HeadTypeDefOf.Grunt_Male);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Ghoul_Narrow, MP_HeadTypeDefOf.Grunt_Male);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Ghoul_Wide, MP_HeadTypeDefOf.Grunt_Male_Heavy);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.CultEscapee, MP_HeadTypeDefOf.Grunt_Male);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.TimelessOne, MP_HeadTypeDefOf.Grunt_Male);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.DarkScholar_Female, MP_HeadTypeDefOf.Grunt_Male);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.DarkScholar_Male, MP_HeadTypeDefOf.Grunt_Male);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Leathery_Female, MP_HeadTypeDefOf.Grunt_Male);
+                HeadTypeCacheMale.Add(MP_HeadTypeDefOf.Leathery_Male, MP_HeadTypeDefOf.Grunt_Male);
+
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Ghoul_Normal, MP_HeadTypeDefOf.Grunt_Female);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Ghoul_Heavy, MP_HeadTypeDefOf.Grunt_Female);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Ghoul_Narrow, MP_HeadTypeDefOf.Grunt_Female);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Ghoul_Wide, MP_HeadTypeDefOf.Grunt_Female_Heavy);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.CultEscapee, MP_HeadTypeDefOf.Grunt_Female);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.TimelessOne, MP_HeadTypeDefOf.Grunt_Female);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.DarkScholar_Female, MP_HeadTypeDefOf.Grunt_Female);
+                HeadTypeCacheFemale.Add(MP_HeadTypeDefOf.Leathery_Female, MP_HeadTypeDefOf.Grunt_Female);
+            }
+        }
+
     }
 }
